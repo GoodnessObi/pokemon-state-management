02291d49842ee0c2633b2f89000adeef47fbdc1c
GoodnessObi <goodnessobi@gmail.com>
2023-05-04 17:46:13 +0100

    feat: create details page and use ReactLocation

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,37 @@ import PokemonList from './pokemon/PokemonList';
 import { PokemonProvider } from './pokemon/PokemonProvider';
 import SearchBox from './pokemon/SearchBox';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Outlet, ReactLocation, Router } from '@tanstack/react-location';
+import PokemonDetail from './pokemon/PokemonDetail';
 
 const queryClient = new QueryClient();
+const location = new ReactLocation();
+
+const routes = [
+	{
+		path: '/',
+		element: (
+			<>
+				<SearchBox />
+				<PokemonList />
+			</>
+		),
+	},
+	{
+		path: '/pokemon/:id',
+		element: <PokemonDetail />,
+	},
+];
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<PokemonProvider>
-				<div className='mx-auto max-w-3xl'>
-					<SearchBox />
-					<PokemonList />
-				</div>
+				<Router location={location} routes={routes}>
+					<div className='mx-auto max-w-3xl'>
+						<Outlet />
+					</div>
+				</Router>
 			</PokemonProvider>
 		</QueryClientProvider>
 	);
